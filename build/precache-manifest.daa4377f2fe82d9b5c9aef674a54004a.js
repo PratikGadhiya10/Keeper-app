e3af08b68a78c2fd12eb0c54cdec3bbc7ed3e59f
unknown <pratikgadhiya50@gmail.com>
2022-05-03 16:29:47 +0530

    heroku app deploy

---
+++ build/precache-manifest.daa4377f2fe82d9b5c9aef674a54004a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ffbc97815be115cfd47617969e6dcdf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1b017533e6e40d3693f",
+    "url": "/static/js/2.8a02f188.chunk.js"
+  },
+  {
+    "revision": "8502a5aac2d603aba46b",
+    "url": "/static/js/main.cedfdd9b.chunk.js"
+  },
+  {
+    "revision": "31cb9d8849daaa195195",
+    "url": "/static/js/runtime-main.028bd04c.js"
+  }
+]);
